6308b926d05c35f4364a79d98533c3f26aa3215d
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-11-11 16:09:50 +0100

    Initial Commit

---
+++ avent-front/src/app/daily-link-list/daily-link-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Link } from '../model/link';
+import { DataserviceService } from '../services/dataservice.service';
+
+@Component({
+  selector: 'app-daily-link-list',
+  templateUrl: './daily-link-list.component.html',
+  styleUrls: ['./daily-link-list.component.css']
+})
+export class DailyLinkListComponent implements OnInit {
+
+  dailyLinks: Link[];
+
+  constructor(private dataService: DataserviceService) { }
+
+  ngOnInit() {
+    this.dataService.getDailyLinks().subscribe( links => this.dailyLinks = links );
+    // Autre
+    //this.dataService.getDailyLinks().subscribe(function onSucess(links)) {
+    // this.dailyLinks = links;
+   // }
+  }
+
+}
